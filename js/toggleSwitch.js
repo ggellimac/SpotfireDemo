db70729028c8c82a03eb981fcb8b952a8d273a28
lcarlso2 <43653618+lcarlso2@users.noreply.github.com>
2019-07-02 13:23:05 -0400

    Setup the toggle switch for the tasks

--- js/toggleSwitch.js
+++ js/toggleSwitch.js
@@ -1,12 +1,37 @@
-document.getElementById("myCheck").addEventListener("click", handleClick);
+document.getElementById("risksAndIssuesToggle").addEventListener("click", handleClickForRisksAndIssues);
+document.getElementById("tasksToggle").addEventListener("click", handleClickForTasks);
 
-function handleClick() {
+//Handles the event when the user toggles the risks and issues toggle switch
+function handleClickForRisksAndIssues() {
     //get the checkbox
-    var checkBox = document.getElementById("myCheck");
+    var checkBox = document.getElementById("risksAndIssuesToggle");
 
     //this is for if you want to grab "all" or "mine"
     var x = document.getElementById("all-content");
     var y = document.getElementById("mine-content");
+
+    //the default toggle setting is set to "all", so if they set it to "mine", show "mine-content" and hide "all-content"
+    if(checkBox.checked === true){
+      y.style.display = "block";
+      x.style.display = "none";
+
+      console.log(x.innerHTML)
+
+    //else show "all-content" and hide "mine-content"
+    } else {
+      x.style.display = "block";
+      y.style.display = "none";
+    }
+}
+
+//Handles the event when the user toggles the tasks toggle switch
+function handleClickForTasks() {
+    //get the checkbox
+    var checkBox = document.getElementById("tasksToggle");
+
+    //this is for if you want to grab "all" or "mine"
+    var x = document.getElementById("all-tasks");
+    var y = document.getElementById("my-tasks");
 
     //the default toggle setting is set to "all", so if they set it to "mine", show "mine-content" and hide "all-content"
     if(checkBox.checked === true){
