de091dc60285f41659ac34c51148c3c54bdf78df
ggellimac <32497210+ggellimac@users.noreply.github.com>
2019-07-02 12:44:24 -0400

    can someone do this for me (toggle stuff)

--- js/sb-admin.js
+++ js/sb-admin.js
@@ -3,8 +3,23 @@
 
   // Box toggles will change content
   function changeContent(){
-    var x = document.getElementByID();
-    if()
+    //get the checkbox
+    var checkBox = document.getElementByID("myCheck");
+
+    //this is for if you want to grab "all" or "mine"
+    var x = document.getElementByID("all-content");
+    var y = document.getElementByID("mine-content");
+
+    //the default toggle setting is set to "all", so if they set it to "mine", show "mine-content" and hide "all-content"
+    if(checkBox.checked === true){
+      y.style.display === "block";
+      x.style.display === "none";
+
+    //else show "all-content" and hide "mine-content"
+    } else {
+      x.style.display === "block";
+      y.style.display === "none";
+    }
   }
 
   // Toggle the side navigation
