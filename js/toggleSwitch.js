34d3680dc8102c7646026ba045b5720e998c70c1
lcarlso2 <43653618+lcarlso2@users.noreply.github.com>
2019-07-02 13:28:18 -0400

    Setup the toggle for activity

--- js/toggleSwitch.js
+++ js/toggleSwitch.js
@@ -1,5 +1,6 @@
 document.getElementById("risksAndIssuesToggle").addEventListener("click", handleClickForRisksAndIssues);
 document.getElementById("tasksToggle").addEventListener("click", handleClickForTasks);
+document.getElementById("activityToggle").addEventListener("click", handleClickForActivity);
 
 //Handles the event when the user toggles the risks and issues toggle switch
 function handleClickForRisksAndIssues() {
@@ -14,8 +15,6 @@ function handleClickForRisksAndIssues() {
     if(checkBox.checked === true){
       y.style.display = "block";
       x.style.display = "none";
-
-      console.log(x.innerHTML)
 
     //else show "all-content" and hide "mine-content"
     } else {
@@ -33,14 +32,33 @@ function handleClickForTasks() {
     var x = document.getElementById("all-tasks");
     var y = document.getElementById("my-tasks");
 
-    //the default toggle setting is set to "all", so if they set it to "mine", show "mine-content" and hide "all-content"
+    //the default toggle setting is set to "all", so if they set it to "mine", show "my-tasks" and hide "all-tasks"
     if(checkBox.checked === true){
       y.style.display = "block";
       x.style.display = "none";
 
-      console.log(x.innerHTML)
+    //else show "all-tasks" and hide "mine-tasks"
+    } else {
+      x.style.display = "block";
+      y.style.display = "none";
+    }
+}
 
-    //else show "all-content" and hide "mine-content"
+//Handles the event when the user toggles the activity toggle switch
+function handleClickForActivity() {
+    //get the checkbox
+    var checkBox = document.getElementById("activityToggle");
+
+    //this is for if you want to grab "all" or "mine"
+    var x = document.getElementById("all-activity");
+    var y = document.getElementById("my-activity");
+
+    //the default toggle setting is set to "all", so if they set it to "mine", show "mine-activity" and hide "all-activity"
+    if(checkBox.checked === true){
+      y.style.display = "block";
+      x.style.display = "none";
+
+    //else show "all-activity" and hide "mine-activity"
     } else {
       x.style.display = "block";
       y.style.display = "none";
