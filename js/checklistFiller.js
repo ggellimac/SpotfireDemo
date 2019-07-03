0b4c28b3fe14238c6e7a773b58ff45cc6b770408
angelseay <angelseay99@gmail.com>
2019-07-03 12:18:41 -0400

    new col for announcement dates

--- js/checklistFiller.js
+++ js/checklistFiller.js
@@ -5,6 +5,10 @@ let myRiskMessages = ["This is my risk"];
 let allTaskMessages = ["This is a task", "This is a new task", "This is my task"];
 let myTaskMessages = ["This is my task"];
 
+let allActivityDatesMessages = ["May 29, 2019 08:15:23 am", "May 29, 2019 08:15:23 am", "May 29, 2019 08:15:23 am", "May 29, 2019 08:15:23 am"];
+let myActivityDatesMessages = ["May 29, 2019 08:15:23 am", "May 29, 2019 08:15:23 am"];
+
+
 let allActivityMessages = ["Jack replied to your comment on your published report \"Sponsor Status Report\"", "Data refresh from Gobalto", "Weekly status report exported to Excel", "You started following Lauren on GSK Notes and Tasks"];
 let myActivityMessage = ["Jack replied to your comment on your published report \"Sponsor Status Report\"", "You started following Lauren on GSK Notes and Tasks"];
 
@@ -12,6 +16,10 @@ injectDomElement(allContent, allRiskMessages);
 injectDomElement(myContent, myRiskMessages);
 injectDomElement(allTasks, allTaskMessages);
 injectDomElement(myTasks, myTaskMessages);
+
+injectDomElement(allActivityDates, allActivityDatesMessages);
+injectDomElement(myActivityDates, myActivityDatesMessages);
+
 injectDomElement(allActivity, allActivityMessages);
 injectDomElement(myActivity, myActivityMessage);
 
@@ -28,12 +36,8 @@ function injectDomElement(parent, messages) {
     checkbox.id = "id";
 
     let line = document.createElement("hr");
-    if (messages != allActivityMessages && messages != myActivityMessage) {
-      label.appendChild(checkbox);
-      label.appendChild(span);
-    }
-   
-
+    // label.appendChild(checkbox);
+    // label.appendChild(span);
     label.appendChild(document.createTextNode(message));
     parent.appendChild(line);
     parent.appendChild(label);
