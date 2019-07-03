6f927d708bc7b9975ebd1c33fa285a5d79e87110
lcarlso2 <43653618+lcarlso2@users.noreply.github.com>
2019-07-03 13:04:26 -0400

    Removed the line that was getting added to the top of the card items at the bottom of the screen

--- js/checklistFiller.js
+++ js/checklistFiller.js
@@ -2,15 +2,34 @@
 let allRiskMessages = ["This is a risk", "This is an issue", "This is my risk"];
 let myRiskMessages = ["This is my risk"];
 
-let allTaskMessages = ["This is a task", "This is a new task", "This is my task"];
+let allTaskMessages = [
+  "This is a task",
+  "This is a new task",
+  "This is my task"
+];
 let myTaskMessages = ["This is my task"];
 
-let allActivityDatesMessages = ["May 29, 2019 08:15:23 am", "May 29, 2019 08:15:23 am", "May 29, 2019 08:15:23 am", "May 29, 2019 08:15:23 am"];
-let myActivityDatesMessages = ["May 29, 2019 08:15:23 am", "May 29, 2019 08:15:23 am"];
+let allActivityDatesMessages = [
+  "May 29, 2019 08:15:23 am",
+  "May 29, 2019 08:15:23 am",
+  "May 29, 2019 08:15:23 am",
+  "May 29, 2019 08:15:23 am"
+];
+let myActivityDatesMessages = [
+  "May 29, 2019 08:15:23 am",
+  "May 29, 2019 08:15:23 am"
+];
 
-
-let allActivityMessages = ["Jack replied to your comment on your published report \"Sponsor Status Report\"", "Data refresh from Gobalto", "Weekly status report exported to Excel", "You started following Lauren on GSK Notes and Tasks"];
-let myActivityMessage = ["Jack replied to your comment on your published report \"Sponsor Status Report\"", "You started following Lauren on GSK Notes and Tasks"];
+let allActivityMessages = [
+  'Jack replied to your comment on your published report "Sponsor Status Report"',
+  "Data refresh from Gobalto",
+  "Weekly status report exported to Excel",
+  "You started following Lauren on GSK Notes and Tasks"
+];
+let myActivityMessage = [
+  'Jack replied to your comment on your published report "Sponsor Status Report"',
+  "You started following Lauren on GSK Notes and Tasks"
+];
 
 injectDomElement(allContent, allRiskMessages);
 injectDomElement(myContent, myRiskMessages);
@@ -24,23 +43,25 @@ injectDomElement(allActivity, allActivityMessages);
 injectDomElement(myActivity, myActivityMessage);
 
 function injectDomElement(parent, messages) {
+  let counter = 0;
   messages.forEach(message => {
     let label = document.createElement("label");
     label.className = "checkbox";
     let span = document.createElement("span");
     span.className = "checkmark";
 
- 
-
     let line = document.createElement("hr");
     // if (messages != allActivityMessages && messages != myActivityMessage) {
     //   label.appendChild(checkbox);
     //   label.appendChild(span);
     // }
-   
 
     label.appendChild(document.createTextNode(message));
-    parent.appendChild(line);
+    if (counter !== 0) {
+      parent.appendChild(line);
+      console.log(messages.length - 1);
+    }
     parent.appendChild(label);
+    counter++;
   });
 }
