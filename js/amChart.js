1b35761452730cb611105864a20fc50775850374
ggellimac <32497210+ggellimac@users.noreply.github.com>
2019-07-08 16:53:57 -0400

    barchart

--- js/amChart.js
+++ js/amChart.js
@@ -1,5 +1,5 @@
 // Create chart instance
-var chart = am4core.create("am-chart", am4charts.PieChart);
+var chart = am4core.create("am-chart", am4charts.XYChart);
 
 
 //var container = am4core.create("am-chart", am4core.Container);
@@ -10,6 +10,7 @@ chart.background.fillOpacity = 2;
 chart.background.stroke = am4core.color("#293b56");
 chart.background.strokeOpacity = 2;
 chart.background.strokeWidth = 2;
+chart.marginRight = 400;
 
 
 //chart.parent = container;
@@ -17,34 +18,83 @@ chart.background.strokeWidth = 2;
 // Add data
 chart.data = [{
   "country": "Lithuania",
-  "litres": 501.9
+  "research": 501.9,
+  "marketing": 250,
+  "sales": 199
 }, {
   "country": "Czech Republic",
-  "litres": 301.9
+  "research": 301.9,
+  "marketing": 222,
+  "sales": 251
 }, {
   "country": "Ireland",
-  "litres": 201.1
+  "research": 201.1,
+  "marketing": 170,
+  "sales": 199
 }, {
   "country": "Germany",
-  "litres": 165.8
+  "research": 165.8,
+  "marketing": 122,
+  "sales": 90
 }, {
   "country": "Australia",
-  "litres": 139.9
+  "research": 139.9,
+  "marketing": 99,
+  "sales": 252
 }, {
   "country": "Austria",
-  "litres": 128.3
+  "research": 128.3,
+  "marketing": 85,
+  "sales": 84
 }, {
   "country": "UK",
-  "litres": 99
+  "research": 99,
+  "marketing": 93,
+  "sales": 142
 }, {
   "country": "Belgium",
-  "litres": 60
+  "research": 60,
+  "marketing": 50,
+  "sales": 55
 }, {
   "country": "The Netherlands",
-  "litres": 50
+  "research": 50,
+  "marketing": 42,
+  "sales": 25
 }];
 
 // Add and configure Series
-var pieSeries = chart.series.push(new am4charts.PieSeries());
-pieSeries.dataFields.value = "litres";
-pieSeries.dataFields.category = "country";
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "country";
+categoryAxis.title.text = "Local country offices";
+categoryAxis.renderer.grid.template.location = 0;
+categoryAxis.renderer.minGridDistance = 20;
+
+
+var  valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.title.text = "Expenditure (M)";
+
+// Create series
+var series = chart.series.push(new am4charts.ColumnSeries());
+series.dataFields.valueY = "research";
+series.dataFields.categoryX = "country";
+series.name = "Research";
+series.tooltipText = "{name}: [bold]{valueY}[/]";
+series.stacked = true;
+
+var series2 = chart.series.push(new am4charts.ColumnSeries());
+series2.dataFields.valueY = "marketing";
+series2.dataFields.categoryX = "country";
+series2.name = "Marketing";
+series2.tooltipText = "{name}: [bold]{valueY}[/]";
+series2.stacked = true;
+
+var series3 = chart.series.push(new am4charts.ColumnSeries());
+series3.dataFields.valueY = "sales";
+series3.dataFields.categoryX = "country";
+series3.name = "Sales";
+series3.tooltipText = "{name}: [bold]{valueY}[/]";
+series3.stacked = true;
+
+// Add cursor
+chart.cursor = new am4charts.XYCursor();
