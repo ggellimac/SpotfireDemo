0e6c6f1ed578e875d0d97cee977d0d227323d41c
lcarlso2 <43653618+lcarlso2@users.noreply.github.com>
2019-07-08 15:15:25 -0400

    Commented out some unneeded code in the amchart file

--- js/amChart.js
+++ js/amChart.js
@@ -1,15 +1,14 @@
-// Create chart instance
-var chart = am4core.create("am-chart", am4charts.PieChart);
+//Create chart instance
+let chart = am4core.create("am-chart", am4charts.PieChart);
 
 
 //var container = am4core.create("am-chart", am4core.Container);
 chart.width = am4core.percent(100);
 chart.height = am4core.percent(100);
-chart.background.fill = am4core.color("#293b56");
-chart.background.fillOpacity = 2;
-chart.background.stroke = am4core.color("#293b56");
-chart.background.strokeOpacity = 2;
-chart.background.strokeWidth = 2;
+//chart.background.fill = am4core.color("#293b56");
+// chart.background.stroke = am4core.color("#293b56");
+//chart.background.strokeOpacity = 2;
+//chart.background.strokeWidth = 2;
 chart.background.fillOpacity = 0;
 
 
@@ -47,7 +46,7 @@ chart.data = [{
 }];
 
 // Add and configure Series
-var pieSeries = chart.series.push(new am4charts.PieSeries());
+let pieSeries = chart.series.push(new am4charts.PieSeries());
 pieSeries.dataFields.value = "litres";
 pieSeries.dataFields.category = "country";
 pieSeries.stroke = am4core.color("#ffffff");
