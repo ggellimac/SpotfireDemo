0c3e90f6cac507d28087cbac261994fe686af7a3
lcarlso2 <43653618+lcarlso2@users.noreply.github.com>
2019-07-09 13:01:05 -0400

    Changed the bar chart from amcharts

--- js/amChart.js
+++ js/amChart.js
@@ -121,7 +121,7 @@ var chart = am4core.create("am-chart", am4charts.XYChart);
 
 
 //var container = am4core.create("am-chart", am4core.Container);
-chart.width = am4core.percent(100);
+chart.width = am4core.percent(25);
 chart.height = am4core.percent(100);
 chart.background.fill = am4core.color("#293b56");
 chart.background.fillOpacity = 0;
@@ -135,87 +135,49 @@ chart.marginRight = 400;
 
 // Add data
 chart.data = [{
-  "country": "Lithuania",
-  "research": 501.9,
-  "marketing": 250,
-  "sales": 199
-}, {
-  "country": "Czech Republic",
-  "research": 301.9,
-  "marketing": 222,
-  "sales": 251
-}, {
-  "country": "Ireland",
-  "research": 201.1,
-  "marketing": 170,
-  "sales": 199
-}, {
-  "country": "Germany",
-  "research": 165.8,
-  "marketing": 122,
-  "sales": 90
-}, {
-  "country": "Australia",
-  "research": 139.9,
-  "marketing": 99,
-  "sales": 252
-}, {
-  "country": "Austria",
-  "research": 128.3,
-  "marketing": 85,
-  "sales": 84
-}, {
-  "country": "UK",
-  "research": 99,
-  "marketing": 93,
-  "sales": 142
-}, {
-  "country": "Belgium",
-  "research": 60,
-  "marketing": 50,
-  "sales": 55
-}, {
-  "country": "The Netherlands",
-  "research": 50,
-  "marketing": 42,
-  "sales": 25
+  "topic": "",
+  "total": 501,
+  "spent": 450,
 }];
 
 // Add and configure Series
 var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "country";
-categoryAxis.title.text = "Local country offices";
+categoryAxis.dataFields.category = "topic";
+categoryAxis.title.text = "Patient Enrollment";
+categoryAxis.title.strokeWidth = 1;
+categoryAxis.fontSize = 22;
+categoryAxis.fill = "#ffffff";
 categoryAxis.renderer.grid.template.location = 0;
 categoryAxis.renderer.minGridDistance = 20;
+categoryAxis.renderer.grid.template.disabled = true;
 
 
 var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-valueAxis.title.text = "Expenditure (M)";
+valueAxis.renderer.labels.template.disabled = true;
+valueAxis.renderer.grid.template.disabled = true;
+
 
 // Create series
+
 var series = chart.series.push(new am4charts.ColumnSeries());
-series.dataFields.valueY = "research";
-series.dataFields.categoryX = "country";
-series.name = "Research";
+series.dataFields.valueY = "total";
+series.dataFields.categoryX = "topic";
+series.clustered = false;
+series.name = "Total";
 series.tooltipText = "{name}: [bold]{valueY}[/]";
-series.stacked = true;
 
 var series2 = chart.series.push(new am4charts.ColumnSeries());
-series2.dataFields.valueY = "marketing";
-series2.dataFields.categoryX = "country";
-series2.name = "Marketing";
+series2.dataFields.valueY = "spent";
+series2.dataFields.categoryX = "topic";
+series2.clustered = false;
+series2.name = "Spent";
 series2.tooltipText = "{name}: [bold]{valueY}[/]";
-series2.stacked = true;
 
-var series3 = chart.series.push(new am4charts.ColumnSeries());
-series3.dataFields.valueY = "sales";
-series3.dataFields.categoryX = "country";
-series3.name = "Sales";
-series3.tooltipText = "{name}: [bold]{valueY}[/]";
-series3.stacked = true;
 
 // Add cursor
 chart.cursor = new am4charts.XYCursor();
+chart.cursor.lineX.disabled = true;
+chart.cursor.lineY.disabled = true;
 
 
 
