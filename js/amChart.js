4441bd707fe19747fb419819257091b013a6a551
angelseay <angelseay99@gmail.com>
2019-07-15 10:36:52 -0400

    started adding progress bar icons

--- js/amChart.js
+++ js/amChart.js
@@ -264,29 +264,24 @@ am4core.ready(function () {
   colorSet.saturation = 0.4;
 
   var icon = "M352.821381,363.269231 L356.814871,357.614288 C357.039817,357.295481 357.060717,356.885808 356.86987,356.548308 C356.677923,356.210808 356.305028,356 355.899684,356 L336.099983,356 C335.492793,356 335,356.465231 335,357.038462 L335,369.5 L335,381.961538 C335,382.534769 335.492793,383 336.099983,383 C336.707174,383 337.199967,382.534769 337.199967,381.961538 L337.199967,370.538462 L355.899684,370.538462 C356.305028,370.538462 356.677923,370.327654 356.86987,369.990154 C357.061267,369.652654 357.040367,369.242981 356.814871,368.924173 L352.821381,363.269231 Z M337.199967,368.461538 L337.199967,358.076923 L353.844365,358.076923 L350.584565,362.693404 C350.338168,363.042327 350.338168,363.496654 350.584565,363.845577 L353.844365,368.461538 L337.199967,368.461538 Z"
-        
+
   chart.data = [
     {
       started: true,
       row: "test",
       name: "PREP",
+      bullet: "images/prep.svg",
       fromDate: "2018-01-01",
       toDate: "2018-03-01",
       color: am4core.color("#38A7C8"),
-      initialImage: {
-        svgPath: icon,
-        color: "#fff",
-        width: 30,
-        height: 30,
-        rotation: 170,
-        offsetX: 4,
-        offsetY: -17
-      },
+      image: icon,
+
     },
     {
       started: true,
       row: "test",
       name: "SITE STARTUP",
+      bullet: "images/prep.svg",
       fromDate: "2018-03-01",
       toDate: "2018-05-25",
       color: am4core.color("#38A7C8")
@@ -296,6 +291,8 @@ am4core.ready(function () {
       started: true,
       row: "test",
       name: "SITE ACTIVATION",
+      bullet: "images/prep.svg",
+
       fromDate: "2018-05-25",
       toDate: "2018-06-01",
       color: am4core.color("#38A7C8")
@@ -304,6 +301,8 @@ am4core.ready(function () {
       started: false,
       row: "test",
       name: "SITE IDENTIFICATION",
+      bullet: "images/prep.svg",
+
       fromDate: "2018-06-01",
       toDate: "2018-06-10",
       color: am4core.color("#4C5A71")
@@ -312,6 +311,8 @@ am4core.ready(function () {
       started: false,
       row: "test",
       name: "PATIENT ENROLLMENT",
+      bullet: "images/prep.svg",
+
       fromDate: "2018-06-10",
       toDate: "2018-07-29",
       color: am4core.color("#4C5A71")
@@ -320,6 +321,8 @@ am4core.ready(function () {
       started: false,
       row: "test",
       name: "PATIENT TREATMENT",
+      bullet: "images/prep.svg",
+
       fromDate: "2018-07-29",
       toDate: "2018-09-08",
       color: am4core.color("#4C5A71")
@@ -329,6 +332,8 @@ am4core.ready(function () {
       started: false,
       row: "test",
       name: "PATIENT FOLLOW UP",
+      bullet: "images/prep.svg",
+
       fromDate: "2018-09-08",
       toDate: "2018-10-30",
       color: am4core.color("#4C5A71")
@@ -338,6 +343,8 @@ am4core.ready(function () {
       started: false,
       row: "test",
       name: "CLOSEOUT",
+      bullet: "images/prep.svg",
+
       fromDate: "2018-10-30",
       toDate: "2018-12-30",
       color: am4core.color("#4C5A71")
@@ -359,9 +366,25 @@ am4core.ready(function () {
   dateAxis.strictMinMax = true;
   dateAxis.renderer.tooltipLocation = 0;
 
+
+
+
+  // image.y = am4core.percent(100);
+  // image.propertyFields.href = "bullet";
+
   var series1 = chart.series.push(new am4charts.ColumnSeries());
   series1.columns.template.width = am4core.percent(80);
-  series1.columns.template.tooltipText = "{name}: {openDateX} - {dateX}";
+  var bullet = series1.bullets.push(new am4charts.LabelBullet());
+  var image = bullet.createChild(am4core.Image);
+  image.dy = -10;
+  image.dx = am4core.percent(0);
+  bullet.locationX = 1;
+
+  image.height = am4core.percent(5);
+
+  image.propertyFields.href = "bullet";
+
+  series1.columns.template.tooltipText = "{name}: {openDateX} - {dateX} ";
   series1.dataFields.openDateX = "fromDate";
   series1.dataFields.dateX = "toDate";
   series1.dataFields.categoryY = "row";
@@ -369,6 +392,7 @@ am4core.ready(function () {
   series1.columns.template.propertyFields.stroke = "color";
   categoryAxis.renderer.labels.template.disabled = true;
 
+  
   // var fillModifier = new am4core.LinearGradientModifier();
   // fillModifier.opacities = [1, 1];
   // fillModifier.brightnesses = [.05, .8];
